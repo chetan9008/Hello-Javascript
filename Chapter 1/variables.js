1e9b683c014ee86ea67fd4e7be9246c02961473f
chetan <chetankumar70174577@gmail.com>
2023-09-02 07:40:38 +0530

    Variables in js

---
+++ Chapter 1/variables.js
@@ -0,0 +1,21 @@
+let variable = 2;
+console.log("Variable is " + variable);
+
+var variables = 3;
+console.log("Variables is " + variables);
+
+const constantVariable = 4;
+
+console.log(constantVariable);
+
+let string = `This is a string`;
+console.log(string);
+
+let city = "New york";
+
+//Template literals 
+
+console.log(`City is ${city}`);
+
+let anotherVariable;
+console.log(anotherVariable);
