5e73c7b792c4cb3105a49118063fbebee421bb75
chetan <chetankumar70174577@gmail.com>
2023-09-12 08:01:48 +0530

    operators in js

---
+++ Chapter 1/operator.js
@@ -0,0 +1,43 @@
+// Airthematic Operator 
+
+let variable = 3;
+let variableSecond = 4;
+
+console.log(variable/variableSecond);
+
+console.log(variable%variableSecond);
+
+console.log(variable**variableSecond);
+
+variable **= 2;
+
+console.log(variable);
+
+console.log(++variable);
+
+//Relational Operator 
+
+console.log(2 == '2');
+console.log(2 === '2');
+console.log('100' + 1);
+
+// Logical Operator 
+
+console.log(true || false);
+
+// Bitwise Operator 
+
+console.log(~2);
+
+// Other Operator 
+
+console.log(`Hello` + ` Java`);
+
+let a = (1,2,3);
+console.log(a);
+
+(2<3)?console.log(2):console.log(3);
+
+console.log(typeof(`a`));
+
+console.log(delete a);
