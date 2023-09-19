a57abe8574637852250856765727d17a9d5ee9af
chetan <chetankumar70174577@gmail.com>
2023-09-19 08:09:35 +0530

    conditional statement in js

--- Chapter 2/While_And_Do_while.js
+++ Chapter 2/While_And_Do_while.js
@@ -1,42 +1,42 @@
 const prompt = require('prompt-sync')();
 let i = 0;
-// while(i!=6)
-// {
-//     console.log(i);
-//     i++;
-// }
+while(i!=6)
+{
+    console.log(i);
+    i++;
+}
 
-// let sum = 0;
-// let number = parseInt(prompt('enter the number : '));
-// while (number >= 0) {
-//     sum += number;
-//     number = parseInt(prompt('Enter the number : '));
-// }
-// console.log(`Sum is ${sum}`);
+let sum = 0;
+let number = parseInt(prompt('enter the number : '));
+while (number >= 0) {
+    sum += number;
+    number = parseInt(prompt('Enter the number : '));
+}
+console.log(`Sum is ${sum}`);
 
-// do
-// {
-//     console.log(i);
-//     i++;
-// }while(i!=6);
+do
+{
+    console.log(i);
+    i++;
+}while(i!=6);
 
-// while(1)
-// {
-//     console.log('infinity');
-// }
+while(1)
+{
+    console.log('infinity');
+}
 
-// while(i != 0)
-// {
-//     console.log(`9 * ${i} = ${9*i}`);
-//     i--;
-// }
+while(i != 0)
+{
+    console.log(`9 * ${i} = ${9*i}`);
+    i--;
+}
 
-// state:while (i != 11) {
-//     i++;
-//     if (i == 5)
-//         continue state;
-//     console.log(i);
-// }
+state:while (i != 11) {
+    i++;
+    if (i == 5)
+        continue state;
+    console.log(i);
+}
 
 i = String('B');
 switch (i) {
