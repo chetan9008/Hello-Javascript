d82160000793915915286f5a675381b9c7a08e48
chetan <chetankumar70174577@gmail.com>
2023-10-11 22:32:39 +0530

    symbol in js

---
+++ Chapter 5/symbol.js
@@ -0,0 +1,53 @@
+function symbolFunction() {
+
+    let symbol = Symbol('chetan');
+    let string = 'chetan';
+    let variable = Symbol();
+    console.log(variable);
+    console.log(symbol.description);
+    console.log(typeof symbol);
+    if (string == symbol)
+        console.log('same');
+    else
+        console.log('not same');
+
+    let name = Symbol('name');
+
+    let object = {
+        subject: 'computer',
+        [name]: 'chetan',
+        marks: 90
+    }
+    console.log(object);
+    for (let key in object) {
+        console.log(key);
+    }
+
+    object[name] = "gaurav";
+
+    console.log(object[name]);
+
+    console.log(object);
+}
+function symbolFunction1()
+{
+    let object = {};
+    object.name = 'chetan';
+    object.class= 'bsc';
+    object.email = 'abc@gmail.com';
+    console.log(object);
+    let email = Symbol('email');
+    object[email] = 'xyz@gmail.com';
+    console.log(object);
+
+    let anotherObject = 
+    {
+        name:'chetan',
+        class:'bsc',
+        email:'abc@gmail.com',
+        [email]:'abc@gmail.com'
+    }
+    console.log(anotherObject);
+    console.log(anotherObject.email === anotherObject[email]);
+}
+symbolFunction1();
