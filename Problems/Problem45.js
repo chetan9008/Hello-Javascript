76d2f7128effe3039e53001487e368c01152df52
chetan <chetankumar70174577@gmail.com>
2023-10-13 23:23:24 +0530

    modifying object

--- Problems/Problem45.js
+++ Problems/Problem45.js
@@ -5,22 +5,30 @@ let object = {
     marks: {
         maths: 90,
         computer: 99
+    },
+    years:{
+        high:2020,
+        inter:2022
     }
 }
 
 let person = {
-    age:30
+    age: 30
 }
 
 object.__proto__ = person;
 
 for (let key in object) {
     console.log(object[key]);
+    if (typeof object[key] === 'object') {
+        for (let innerKey in object[key]) {
+            console.log(object[key][innerKey]);
+        }
+    }
 }
 
 console.log();
-for(let [key,value] of Object.entries(object))
-{
+for (let [key, value] of Object.entries(object)) {
     console.log(value);
 }
 
