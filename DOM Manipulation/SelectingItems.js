6d22d101b9bf1727e035e16137be1b52378c3254
chetan <chetankumar70174577@gmail.com>
2023-11-24 21:50:26 +0530

    selecting elements in dom

---
+++ DOM Manipulation/SelectingItems.js
@@ -0,0 +1,35 @@
+// getElementById()
+
+const heading = document.getElementById('heading');
+
+console.log(heading);
+
+//getElementByClassName()
+
+const list = document.getElementsByClassName('list')
+
+console.log(list[0]);
+
+//getElementByTagName()
+
+const tag = document.getElementsByTagName('li')
+
+console.log(tag[1]);
+
+//querySelector()
+
+const item = document.querySelector('#heading');
+
+console.log(item);
+
+//querySelectorAll()
+
+const items = document.querySelectorAll('.list');
+
+console.log(items);
+
+//getElementByName()
+
+const name = document.getElementsByName('chetan');
+
+console.log(name);
